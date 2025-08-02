eed2de3bbb44b45762cef4bd268b65b81c10d8a6
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-08-02 17:53:14 +0600

    Create Offer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Product from "./components/Product";
 import Categories from "./components/Categories";
+import Offers from "./components/Offers";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Product />
       <Categories />
+      <Offers />
     </div>
   );
 }
